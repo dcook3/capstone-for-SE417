27a340c62a5af515363b406cba48b0c8e402c7d6
Lucas Johnson <lucas.johnson02@outlook.com>
2022-01-31 14:02:09 -0500

    me first

--- admin/menu_view.php
+++ admin/menu_view.php
@@ -1,5 +1,5 @@
 <?php
-
+    include("..\models\sql_functions.php")
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +8,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <title>Menu View</title>
+    
 </head>
 <body>
     <h1>Menu Items</h1>
@@ -33,10 +35,46 @@
         <tbody>
             <?php 
                 foreach(getMenuItems() as $item){
-                    
-                }
             ?>
+                    <tr>
+                        
+                        <td><?= $item["item_name"]?></td>
+                        <td><?= $item["section_id"]?></td>
+                        <td><?= $item["item_description"]?></td>
+                        <td><?= $item["item_price"]?></td>
+                        <td><button class = "editButton" data-id = "<?= $item["item_id"]?>">Edit</button></td>
+                        <td><button>Delete</button></td>
+                    </tr>
+            
+            <?php } ?>
         </tbody>
     </table>
+
+    <script>
+        var addNewbutton = document.querySelector("#addNewButton");
+        var editButtons = document.querySelectorAll(".editButton")
+        addNewbutton.addEventListener("click", function(e){
+            window.location.replace("localhost/as_capstone/admin/menu_add.php");
+        })
+
+
+        for(let i = 0; i < editButtons.length; i++){
+            editButtons[i].addEventListener("click", function(e){
+                console.log(e.target.dataset["id"])
+            })
+        }
+        // $.ajax({
+        //     type: "POST",
+        //     url: "../Webservices/EmployeeService.asmx/GetEmployeeOrders",
+        //     data: {id: },
+        //     contentType: "application/json; charset=utf-8",
+        //     dataType: "json",
+        //     success: function(result){
+        //         alert(result.d);
+        //         console.log(result);
+        //     }
+        // });
+
+    </script>
 </body>
 </html>
