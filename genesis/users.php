5d35fd71e272bf8f726e11e2fde5fad36600086b
Genesis <42148976+genesisoliva@users.noreply.github.com>
2022-02-03 07:25:49 -0500

    update

--- genesis/users.php
+++ genesis/users.php
@@ -18,18 +18,18 @@
     <table>
 		<thead>
             <tr>
-                <th colspan="8"><center>Users</center></th>
+                <th colspan="13"><center>Users</center></th>
             </tr>
 			<tr>
                 <th><center>id</center></th>
 
-				<th><center>Student Name</center></th>
 				<th><center>Student ID</center></th>
-				<th><center>Email</center></th>
-				<th><center>Phone #</center></th>
-				<th><center>Phone 2</center></th>
 				<th><center>Dorm</center></th>
-                <th colspan="2"><center>***</center></th>
+				<th colspan="3"><center>Name</center></th>
+				<th colspan="2"><center>Phone</center></th>
+				
+				<th colspan="2"><center>Email</center></th>
+                <th colspan="3"><center>Action</center></th>
 			</tr>
 		</thead>
 		<tbody>
@@ -37,16 +37,19 @@
             <?php foreach ($users as $row): ?>
             <tr>
                 <td><?= $row['id'];?></td>
-
+<td><?= $row['student_id'];?></td>
                 <td><?= $row['student_id'];?></td>
-                <td><?= $row['first_name'];?> <?= $row['last_name'];?></td>
+                <td><?= $row['first_name'];?></td>
+				<td><?= $row['middle_name'];?></td>
+				<td><?= $row['last_name'];?></td>
                 <td><?= $row['phone'];?></td>
                 <td><?= $row['phone2'];?></td>
                 <td><?= $row['email'];?></td>
-                <td><?= $row['dorm_num'];?></td>
-
-                <td><a href="user.php?id=<?=$row['id']?>&action=update"></a></td>
-                <td></td>
+				<td><?= $row['email2'];?></td>
+       
+				<td>View</td>
+                <td><a href="user.php?id=<?=$row['id']?>&action=update">Edit</a></td>
+                <td>Delete</td>
             </tr>
             <?php endforeach; ?>
 
