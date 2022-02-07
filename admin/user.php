99155995ae2aeb376b5328f773f1951bcca76740
“genesisoliva” <gjoliva@email.neit.edu>
2022-02-06 21:16:46 -0500

    update

--- admin/user.php
+++ admin/user.php
@@ -6,18 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<?php include __DIR__ . '/functions.php'; ?>
+<?php include '../models/sql_functions.php'; ?>
 <body>
 
 <form class="form-horizontal" action="user.php" method="post">
             <h2><?=ucWords($action);?> User</h2>
-            <input type="text" name="action" value="<?=$action;?>" hidden>
-            <input type="text" name="id" value="<?=$id;?>" hidden> 
+            <input type="text" name="action" value="<?=$action;?>" >
+            <input type="text" name="id" value="<?=$id;?>" > 
 
             <div class="form-group">
-                <label class="control-label col-sm-2" for="firstName">Student ID:</label>
+                <label class="control-label col-sm-2">Student ID:</label>
                 <div class="col-sm-5">
-                    <input type="text" class="form-control" id="firstName" placeholder="" name="student_id" value="<?=$student_id;?>">
+                    <input type="number" class="form-control" name="studentID" value="<?=$student_id;?>" required>
                 </div>
             </div>
             <?php
@@ -25,28 +25,34 @@
                         {
                     ?>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="firstName">Dorm Number:</label>
+                <label class="control-label col-sm-2" >Dorm Number:</label>
                 <div class="col-sm-5">
-                    <input type="text" class="form-control" id="firstName" placeholder="" name="dorm_num" value="<?=$dorm_no;?>">
+                    <input type="text" class="form-control"  name="dorm_num" value="<?=$dorm_num;?>">
                 </div>
             </div>
             <?php } ?>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="firstName">First Name:</label>
+                <label class="control-label col-sm-2">First Name:</label>
                 <div class="col-sm-5">
-                    <input type="text" class="form-control" id="firstName" placeholder="" name="firstName" value="<?=$first_name;?>">
+                    <input type="text" class="form-control" name="firstName" value="<?=$first_name;?>" required>
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="last name">Last Name:</label>
+                <label class="control-label col-sm-2" >Middle Name:</label>
                 <div class="col-sm-5">
-                    <input type="text" class="form-control" id="lastName" placeholder="" name="lastName" value="<?=$last_name;?>">
+                    <input type="text" class="form-control"  name="middleName" value="<?=$middle_name;?>">
                 </div>
             </div>
             <div class="form-group">
-                <label class="control-label col-sm-2" for="last name">Phone:</label>
+                <label class="control-label col-sm-2" >Last Name:</label>
                 <div class="col-sm-5">
-                    <input type="text" class="form-control" id="lastName" placeholder="" name="phone" value="<?=$phone;?>">
+                    <input type="text" class="form-control"  name="lastName" value="<?=$last_name;?>" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="control-label col-sm-2" >Phone:</label>
+                <div class="col-sm-5">
+                    <input type="text" class="form-control" name="phone" value="<?=$phone;?>" required>
                 </div>
             </div>
             <?php
@@ -63,9 +69,20 @@
             <div class="form-group">
                 <label class="control-label col-sm-2">Email</label>
                 <div class="col-sm-5">
-                    <input type="email" class="form-control"  name="email2" value="<?=$email;?>">
+                    <input type="email" class="form-control"  name="email" value="<?=$email;?>" required>
                 </div>
             </div>
+            <?php
+                        if($action == "update")
+                        {
+                    ?>
+            <div class="form-group">
+                <label class="control-label col-sm-2">Email 2:</label>
+                <div class="col-sm-5">
+                    <input type="email" class="form-control" name="email2" value="<?=$email2;?>">
+                </div>
+            </div>
+            <?php } ?>
             <?php
                         if($action == "add")
                         {
@@ -73,19 +90,19 @@
             <div class="form-group">
                 <label class="control-label col-sm-2">Password</label>
                 <div class="col-sm-5">
-                    <input type="password" class="form-control" name="password" value="<?=$password;?>">
+                    <input type="password" class="form-control" name="password" value="<?=$password;?>" required>
                 </div>
             </div>
 <?php } ?>
             <div class="form-group">
                 <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" name="btnSubmit" class="btn btn-default"><?=ucWords($action);?> Student</button>
+                <button type="submit" name="btnSubmit" class="btn btn-default"><?=ucWords($action);?></button>
 
                     <?php
                         if($action == "update")
                         {
                     ?>
-                        <button type="submit" name="btnDelete" class="btn btn-default" <?=$action=="update"?"":"hidden";?>>Delete Student</button>
+                        <button type="submit" name="btnDelete" class="btn btn-default" <?=$action=="update"?"":"hidden";?>>Delete</button>
                     <?php
                         }
  
--- models/genesis.php
+++ models/genesis.php
@@ -6,7 +6,7 @@ function getStudents () {
     
     $results = [];
 
-    $stmt = $db->prepare("SELECT * FROM users ORDER BY id"); 
+    $stmt = $db->prepare("SELECT * FROM users ORDER BY user_id"); 
     
     if ( $stmt->execute() && $stmt->rowCount() > 0 ) {
          $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -16,14 +16,15 @@ function getStudents () {
      return ($results);
 }
 
-function addStudent($student_id, $first_name, $last_name, $phone, $email, $password) {
+function addStudent($student_id, $first_name, $middle_name, $last_name, $phone, $email, $password) {
     global $db;
     $results = "Not added";
 
-    $stmt = $db->prepare("INSERT INTO users SET student_id = :student_id, first_name = :first_name, last_name = :last_name, phone = :phone, email = :email, password = :password");
+    $stmt = $db->prepare("INSERT INTO users SET student_id = :student_id, first_name = :first_name, middle_name = :middle_name, last_name = :last_name, phone = :phone, email = :email, password = :password");
 
     $stmt->bindValue(':student_id', $student_id);
     $stmt->bindValue(':first_name', $first_name);
+    $stmt->bindValue(':middle_name', $middle_name);
     $stmt->bindValue(':last_name', $last_name);
     $stmt->bindValue(':phone', $phone);
     $stmt->bindValue(':email', $email);
@@ -43,9 +44,9 @@ function getStudent($id){
         
         $results = [];
 
-        $stmt = $db->prepare("SELECT * FROM users WHERE id=:id"); 
+        $stmt = $db->prepare("SELECT * FROM users WHERE user_id=:user_id"); 
 
-        $stmt->bindValue(':id', $id);
+        $stmt->bindValue(':user_id', $id);
         
         if ( $stmt->execute() && $stmt->rowCount() > 0 ) {
              $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -54,23 +55,25 @@ function getStudent($id){
          return ($results);
 }
 
-function updateStudent($id, $img, $student_id, $dorm_num, $first_name, $middle_name, $last_name, $phone, $phone2, $email) {
+function updateStudent($id, $img, $student_id, $dorm_num, $first_name, $middle_name, $last_name, $phone, $phone2, $email, $email2) {
     global $db;
 
     $results = "Data NOT Updated";
     
     $stmt = $db->prepare("UPDATE users SET img = :img, student_id = :student_id, dorm_num = :dorm_num, first_name = :first_name,
-    last_name = :last_name, phone = :phone, phone2 = :phone2, email = :email WHERE id=:id");
+    middle_name = :middle_name, last_name = :last_name, phone = :phone, phone2 = :phone2, email = :email, email2 = :email2 WHERE user_id=:user_id");
     
-    $stmt->bindValue(':id', $id);
+    $stmt->bindValue(':user_id', $id);
     $stmt->bindValue(':img', $img);
     $stmt->bindValue(':student_id', $student_id);
     $stmt->bindValue(':dorm_num', $dorm_num);
     $stmt->bindValue(':first_name', $first_name);
+    $stmt->bindValue(':middle_name', $middle_name);
     $stmt->bindValue(':last_name', $last_name);
     $stmt->bindValue(':phone', $phone);
     $stmt->bindValue(':phone2', $phone2);
     $stmt->bindValue(':email', $email);
+    $stmt->bindValue(':email2', $email2);
 
   
     if ($stmt->execute() && $stmt->rowCount() > 0) {
@@ -85,13 +88,101 @@ function deleteStudent ($id) {
     
     $results = "Data was not deleted";
 
-    $stmt = $db->prepare("DELETE FROM User_Login WHERE id=:id");
+    $stmt = $db->prepare("DELETE FROM users WHERE user_id=:user_id");
     
-    $stmt->bindValue(':id', $id);
+    $stmt->bindValue(':user_id', $id);
         
     if ($stmt->execute() && $stmt->rowCount() > 0) {
         $results = 'Data Deleted';
     }
     
     return ($results);
+}
+?>
+
+<?php
+function isPostRequest() {
+    return ( filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST' );
+    }
+
+if(isset($_GET['id']))
+    {
+        $id = filter_input(INPUT_GET, 'id');
+        $action = filter_input(INPUT_GET, 'action');
+
+        $row = getStudent($id);
+
+        $img= $row[0]['img'];
+        $student_id = $row[0]['student_id'];
+        $dorm_num = $row[0]['dorm_num'];
+        $first_name = $row[0]['first_name'];
+		$middle_name = $row[0]['middle_name'];
+        $last_name = $row[0]['last_name'];
+        $phone = $row[0]['phone'];
+        $phone2 = $row[0]['phone2'];
+        $email = $row[0]['email'];
+        $email2 = $row[0]['email2'];
+        $password = $row[0]['password'];
+    }
+    else 
+    {
+        $id = "";
+        $action = filter_input(INPUT_GET, 'action');
+
+        $img = "";
+        $student_id = "";
+        $dorm_num = "";
+        $first_name = "";
+		$middle_name = "";
+        $last_name = "";
+        $phone = "";
+        $phone2 = "";
+        $email = "";
+        $email2 = "";
+        $password = "";
+    }
+
+
+
+    if(isset($_POST['action'])) 
+    {
+        $action = filter_input(INPUT_POST, 'action');
+
+        if(isPostRequest())
+        {
+            $img = filter_input(INPUT_POST, 'img');
+            $student_id = filter_input(INPUT_POST, 'studentID');
+            $dorm_num = filter_input(INPUT_POST, 'dorm_num');
+            $first_name = filter_input(INPUT_POST, 'firstName');
+			$middle_name = filter_input(INPUT_POST, 'middleName');
+            $last_name = filter_input(INPUT_POST, 'lastName');
+            $phone = filter_input(INPUT_POST, 'phone');
+            $phone2 = filter_input(INPUT_POST, 'phone2');
+            $email = filter_input(INPUT_POST, 'email');
+            $email2 = filter_input(INPUT_POST, 'email2');
+            $password = filter_input(INPUT_POST, 'password');
+    
+            if($action == "add")
+            {
+                $results = addStudent($student_id, $first_name, $middle_name, $last_name, $phone, $email, $password);
+            }
+
+            else if($action == "update")
+            {
+                $id = filter_input(INPUT_POST, 'id');
+    
+                if(isset($_POST['btnDelete']))
+                {
+                    $results = deleteStudent($id);
+                }
+                
+                else if(isset($_POST['btnSubmit']))
+                {
+                    $results = updateStudent($id, $img, $student_id, $dorm_num, $first_name, $middle_name, $last_name, $phone, $phone2, $email, $email2);
+                }
+                
+            }
+
+        }
+
 }
