040f007d26033162dc370e96feb74c2fb43b52fe
Dylan Cook <dcook@email.neit.edu>
2022-02-06 21:20:15 -0500

    untested functionality for orders view complete

--- admin/orders_view.php
+++ admin/orders_view.php
@@ -20,13 +20,26 @@ if($_SERVER['REQUEST_METHOD'] == "GET")
         $todayDate = getdate($todayDate->getTimestamp());
         $selectedYear = $todayDate["year"];
         $selectedMonth = $todayDate['month'];
-        $selectedMonthInt = date_parse($currentMonth)['month'];
+        $selectedMonthInt = date_parse($selectedMonth)['month'];
         $selectedDay = $todayDate['mday'];
         $selectedDate = strtotime("{$selectedYear}/{$selectedMonthInt}/{$selectedDay}");
         $results = $tempOrder->getOrdersByDT($selectedDate);
     }
 }
+
+if(isset($_GET['updateOrderStatus']))
+{
+    $updateOID = $_GET['orderID'];
+
+    //Use update method on a temporary order instance
+}
+
 ?>
+<style>
+    table, td{
+        border: 1px solid black;
+    }
+</style>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,13 +47,14 @@ if($_SERVER['REQUEST_METHOD'] == "GET")
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Orders View</title>
+    <script src="https://kit.fontawesome.com/4933cad413.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <form method="get" action="orders_view.php" id="dateFilter">
         <select class="dateFilter" name="year">
             <option disabled selected hidden>Year</option>
             <?php 
-                for($i =$currentYear ; $i>=$currentYear-40; $i--)
+                for($i =$selectedYear ; $i>=$selectedYear-40; $i--)
                 {
                     if($i == 1 && !isset($_GET['year']))
                     {
@@ -81,25 +95,25 @@ if($_SERVER['REQUEST_METHOD'] == "GET")
             <?php else: ?>
                 <option disabled selected hidden>Month</option>
             <?php endif; ?>
-            <option>January</option>
-            <option>Feburary</option>
-            <option>March</option>
-            <option>April</option>
-            <option>May</option>
-            <option>June</option>
-            <option>July</option>
-            <option>August</option>
-            <option>September</option>
-            <option>October</option>
-            <option>November</option>
-            <option>Decemeber</option>
+            <option value=1>1</option>
+            <option value=2>2</option>
+            <option value=3>3</option>
+            <option value=4>4</option>
+            <option value=5>5</option>
+            <option value=6>6</option>
+            <option value=7>7</option>
+            <option value=8>8</option>
+            <option value=9>9</option>
+            <option value=10>10</option>
+            <option value=11>11</option>
+            <option value=12>12</option>
         </select>
         <select name="day" class="dateFilter">
             <?php
             if(isset($_GET['day']))
             {
                 echo "<option selected hidden>{$_GET['day']}</option>";
-                for($i = 1; $i <= cal_days_in_month(CAL_GREGORIAN, date_parse($_GET['month'])['month'], $_GET['year']); $i++)
+                for($i = 1; $i <= cal_days_in_month(CAL_GREGORIAN, $_GET['month'], $_GET['year']); $i++)
                 {
                     echo "<option>{$i}</option>";
                 }
@@ -108,7 +122,7 @@ if($_SERVER['REQUEST_METHOD'] == "GET")
             {
                 if(isset($_GET['year']) && isset($_GET['month']))
                 {
-                    for($i = 1; $i <= cal_days_in_month(CAL_GREGORIAN, date_parse($_GET['month'])['month'], $_GET['year']); $i++)
+                    for($i = 1; $i <= cal_days_in_month(CAL_GREGORIAN, $_GET['month'], $_GET['year']); $i++)
                     {
                         echo "<option>{$i}</option>";
                     }
@@ -131,15 +145,47 @@ if($_SERVER['REQUEST_METHOD'] == "GET")
             <th>Complete</th>
         </thead>
         <tbody>
-            <?php foreach($results[0] as $row): ?>
-                <tr>
-                    <td><?= "{$row['first_name']} {$row['last_name']}" ?></td>
-                    <td><?= $row['student_id'] ?></td>
-                    <td>soon to be implemented</td>
-                    <td></td>
-                    <!-- PHP call to second query withing index 1 on result array-->
-                </tr>
-            <?php endforeach; ?>
+            <?php if(gettype($results) != "string"): ?>
+                <?php foreach($results as $row): ?>
+                    <tr>
+                        <td><?= "{$row['first_name']} {$row['last_name']}" ?></td>
+                        <td><?= $row['student_id']; ?></td>
+                        <td>soon to be implemented</td>
+                        <td>
+                            <?php 
+                                $tempOrder = new Order();
+                                $tempOrder->populateOrderByID();
+                                $out = "<ul>";
+                                foreach($tempOrder->menu_items as $item)
+                                {
+                                    $out += "<li>$item->item_name";
+                                    $out += "<ul>";
+                                    foreach($item->itemIngredients as $ingredient)
+                                    {
+                                        $out += "<li>$ingredient->ingredientName</li>";
+                                    }
+                                    $out += "</ul>";
+                                }
+                                $out += "</ul>";
+                                echo $out;
+                            ?>
+                        </td>
+                        <td><a href="orders_view.php?orderID=<?= $row['order_id']; ?>"><i class="fas fa-trash-alt"></i></a></td>
+                        <td>
+                            <?php
+                                if($row['order_status'] == "0")
+                                {
+                                    echo "<form action='orders_view.php'><input type='hidden' name='orderID' value='true' /><input type='check' name='updateOrderStatus' /></form>";
+                                }
+                                else if($row['order_status'] == "1")
+                                {
+                                    echo "<form action='orders_view.php'><input type='hidden' name='orderID' value='true' /><input checked type='check' name='updateOrderStatus' /></form>";
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php endif; ?>
         </tbody>
     </table>
 
@@ -159,6 +205,18 @@ if($_SERVER['REQUEST_METHOD'] == "GET")
         else
         {
             selects[2].disabled = false;
+        }
+
+        var itemLinks = document.querySelector('.orderItems');
+
+        if(itemLinks != null)
+        {
+            for(let i=0; i<itemLinks.length; i++)
+            {
+                itemLinks[i].addEventListener(`click`, e => {
+                    //This will trigger the showing/hiding of the context box once functionality is implemented
+                });
+            }
         }
     </script>
 </body>
--- models/dylan.php
+++ models/dylan.php
@@ -5,27 +5,30 @@ include 'db.php';
 // PHP Date -> SQL Date
 // $mysqldate = date( 'Y-m-d H:i:s', $phpdate );
 
-class order 
+class Order 
 {
-    private $order_items, $first_name, $last_name, $total_price;
-    
+    private $orderID, $first_name, $last_name, $total_price;
+    private $order_items, $menu_items;
+
     public function getOrdersByDT($selectedTS)
     {
         global $db;
         $results = [];
 
+        //SQL tables got changed around double check names
+        $SQL = $db->prepare("SELECT * FROM orders INNER JOIN users ON users.user_id = orders.user_id WHERE order_datetime > :datestart AND order_datetime < :dateend ORDER BY order_status ASC, order_datetime ASC;");
+
         $SQLdateStart = new DateTime(date( 'Y-m-d H:i:s', $selectedTS));
         $SQLdateEnd = date( 'Y-m-d H:i:s', $SQLdateStart->add(new DateInterval('P1D'))->getTimestamp());
 
-        //SQL tables got changed around double check names
-        $SQL = $db->prepare("SELECT * FROM orders INNER JOIN users ON users.user_id = orders.user_id WHERE order_datetime > {$SQLdateStart} AND order_datetime < {$SQLdateEnd} ORDER BY order_status ASC, order_datetime ASC;");
-        
-        if($SQL->execute() && $SQL->rowCount() > 0)
+        $binds = array(
+            ":datestart" => date( 'Y-m-d H:i:s', $SQLdateStart->getTimestamp()),
+            ":dateend" => $SQLdateEnd
+        );
+
+        if($SQL->execute($binds) && $SQL->rowCount() == 1)
         {
-            $results = $SQL->fetchAll(PDO::FETCH_ASSOC);
-            $first_name = $results['first_name'];
-            $last_name = $results['last_name'];
-            
+            $results = $SQL->fetchAll(PFO::FETCH_ASSOC);
         }
         else
         {
@@ -33,5 +36,61 @@ class order
         }
 
         return ($results);
+    }
+
+    public function populateOrderByID($orderID)
+    {
+        global $db;
+        $results = [];
+
+        //SQL tables got changed around double check names
+        $SQL = $db->prepare("SELECT * FROM orders WHERE order_id = :oid;");
+
+        $SQL->bindValue(":oid", $order_id);
+        
+
+        if($SQL->execute($binds) && $SQL->rowCount() == 1)
+        {
+            $results = $SQL->fetchAll(PDO::FETCH_ASSOC);
+            $this->orderID = $results["order_id"];
+            $this->first_name = $results["first_name"];
+            $this->last_name = $results["last_name"];
+            $this->order_items = getOrderItemsByOID($this->orderID);
+            $menuItems = [];
+            foreach($this->order_items as $OI)
+            {
+                $item = new Menu_Item(getMenuItemByID($OI["item_id"]));
+                $menuItems->array_push($item);
+            }
+            return [$results, $menuItems];
+        }
+        else
+        {
+            return "A SQL error occured on fetching Order from server with id $orderID.";
+        }
+    }
+}
+
+class Order_Item
+{
+    private $order_item_id, $order_id, $item_id, $qty;
+
+    public static function getOrderItemsByOID($orderID)
+    {
+        global $db;
+        $results = [];
+
+        $SQL = $db->prepare("SELECT * FROM order_items WHERE order_id = :oid;");
+
+        $SQL->bindValue(":oid", $orderID, PDO::PARAM_INT);
+
+        if($SQL->execute() && $SQL->rowCount() > 0)
+        {
+            $results = $SQL->fetchAll(PDO::FETCH_ASSOC);
+        }
+        else
+        {
+            $results = "A SQL error occured on fetching Orders Items from server.";
+        }
     }
 }
