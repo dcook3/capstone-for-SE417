3e55880e5c3b1ef680849b54f1c4f713c74ad7f2
Oliva <gjoliva@email.neit.edu>
2022-02-14 13:43:38 -0500

    update

--- admin/add.php
+++ admin/add.php
@@ -1,153 +1,90 @@
 <?php
+$levels = 1;
 include '../include/header.php';
 Session::CheckSession();
-
- ?>
+$sId =  Session::get('roleid');
+if ($sId === '1') { ?>
 
 <?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['addUser'])) {
 
-if (isset($_GET['id'])) {
-  $userid = preg_replace('/[^a-zA-Z0-9-]/', '', (int)$_GET['id']);
-
+  $userAdd = $users->addNewUserByAdmin($_POST);
 }
 
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update'])) {
-  $updateUser = $users->updateUserByIdInfo($userid, $_POST);
-
+if (isset($userAdd)) {
+  echo $userAdd;
 }
-if (isset($updateUser)) {
-  echo $updateUser;
-}
-
-
-
-
  ?>
+
 
  <div class="card ">
    <div class="card-header">
-          <h3>User Profile <span class="float-right"> <a href="users.php" class="btn btn-primary">Back</a> </h3>
+          <h3 class='text-center'>Add New User</h3>
         </div>
-        <div class="card-body">
-
-    <?php
-    $getUinfo = $users->getUserInfoById($userid);
-    if ($getUinfo) {
+        <div class="cad-body">
 
 
 
+            <div style="width:600px; margin:0px auto">
 
-
-
-     ?>
-
-
-          <div style="width:600px; margin:0px auto">
-
-          <form class="" action="" method="POST">
-          <div class="form-group">
-                <label for="username">Your username</label>
-                <input type="text" name="username" value="<?php echo $getUinfo->student_id; ?>" class="form-control">
-              </div>
-              <div class="form-group">
-                <label for="first_name">Your first name</label>
-                <input type="text" name="first_name" value="<?php echo $getUinfo->first_name; ?>" class="form-control">
-              </div>
-              <div class="form-group">
-                <label for="last_name">Your last name</label>
-                <input type="text" name="last_name" value="<?php echo $getUinfo->last_name; ?>" class="form-control">
-              </div>
-              <div class="form-group">
-                <label for="phone">Phone</label>
-                <input type="text" id="phone" name="phone" value="<?php echo $getUinfo->phone; ?>" class="form-control">
-              </div>
-              <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" id="email" name="email" value="<?php echo $getUinfo->email; ?>" class="form-control">
-              </div>
-
-              <?php if (Session::get("roleid") == '1') { ?>
-
-              <div class="form-group
-              <?php if (Session::get("roleid") == '1' && Session::get("id") == $getUinfo->id) {
-                echo "d-none";
-              } ?>
-              ">
-                <div class="form-group">
-                  <label for="sel1">Select user Role</label>
-                  <select class="form-control" name="roleid" id="roleid">
-
-                  <?php
-
-                if($getUinfo->roleid == '1'){?>
-                  <option value="1" selected='selected'>Admin</option>
-                  <option value="2">Editor</option>
-                  <option value="3">User only</option>
-                <?php }elseif($getUinfo->roleid == '2'){?>
-                  <option value="1">Admin</option>
-                  <option value="2" selected='selected'>Editor</option>
-                  <option value="3">User only</option>
-                <?php }elseif($getUinfo->roleid == '3'){?>
-                  <option value="1">Admin</option>
-                  <option value="2">Editor</option>
-                  <option value="3" selected='selected'>User only</option>
-
-
-                <?php } ?>
-
-
-                  </select>
+            <form action="" method="post">
+            <div class="form-group pt-3">
+                  <label for="username">Your username</label>
+                  <input type="text" name="username"  class="form-control">
                 </div>
-              </div>
-
-          <?php }else{?>
-            <input type="text" name="roleid" value="<?php echo $getUinfo->roleid; ?>"hidden>
-          <?php } ?>
-
-              <?php if (Session::get("id") == $getUinfo->user_id) {?>
-
-
-              <div class="form-group">
-                <button type="submit" name="update" class="btn btn-success">Update</button>
-                <a class="btn btn-primary" href="password.php?id=<?php echo $getUinfo->user_id;?>">Password change</a>
-              </div>
-            <?php } elseif(Session::get("roleid") == '1') {?>
-
-
-              <div class="form-group">
-                <button type="submit" name="update" class="btn btn-success">Update</button>
-                <a class="btn btn-primary" href="password.php?id=<?php echo $getUinfo->user_id;?>">Password change</a>
-              </div>
-            <?php } elseif(Session::get("roleid") == '2') {?>
-
-
-              <div class="form-group">
-                <button type="submit" name="update" class="btn btn-success">Update</button>
-
-              </div>
-
-              <?php   }else{ ?>
+                <div class="form-group">
+                  <label for="first_name">Your first name</label>
+                  <input type="text" name="first_name"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="last_name">Your last name</label>
+                  <input type="text" name="last_name"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="phone">Phone</label>
+                  <input type="text" name="phone"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="email">Email</label>
+                  <input type="email" name="email"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" name="password" class="form-control">
+                </div>
+                <div class="form-group">
                   <div class="form-group">
+                    <label for="sel1">Select user Role</label>
+                    <select class="form-control" name="roleid" id="roleid">
+                      <option value="1">Admin</option>
+                      <option value="2">Editor</option>
+                      <option value="3">User only</option>
 
-                    <a class="btn btn-primary" href="users.php">Ok</a>
+                    </select>
                   </div>
-                <?php } ?>
+                </div>
+                <div class="form-group">
+                  <button type="submit" name="addUser" class="btn btn-success">Register</button>
+                </div>
 
 
-          </form>
+            </form>
+          </div>
+
+
         </div>
-
-      <?php }else{
-
-        header('Location:users.php');
-      } ?>
-
-
-
       </div>
-    </div>
 
+<?php
+}
+else{
+
+  header('Location:users.php');
+
+
+
+}
+ ?>
 
   <?php
   include '../include/footer.php';
